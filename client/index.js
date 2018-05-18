b2980675f31437b8ba800fb7760355657e83815f
Andrew Eames <gamerweazel@gmail.com>
2018-05-18 16:22:54 -0600

    Vue js bare minimum starter code.

---
+++ client/index.js
@@ -0,0 +1,9 @@
+// Vue instance. Controls the app logic.
+new Vue({
+  // el, short for element. 'Hooks' into the div on index.html page
+  el: '#app',
+  // Data is the stuff our app cares about. We usually want to show this on the page.
+  data: {
+    message: 'Hello World!'
+  }
+})
