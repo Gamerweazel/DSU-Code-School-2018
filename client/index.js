1a57f2f33d68ca4d118796be7fecbd6a7eef3ef3
Andrew Eames <gamerweazel@gmail.com>
2018-05-21 13:11:56 -0600

    Prepend to a list and clear the inputs.

--- client/index.js
+++ client/index.js
@@ -13,7 +13,10 @@ const app = new Vue({
       // We have access to our data items by prepending 'this' before the name.
       // This code will add to our expenses list according to what was currently in the inputs when the user saved them.
       // Because description and amount were bound to inputs with v-model, they will contain whatever text is in the input.
-      this.expenses.push({ description: this.description, amount: this.amount })
+      this.expenses.unshift({ description: this.description, amount: this.amount })
+      // Remember we can clear the inputs manually.
+      this.description = ''
+      this.amount = ''
     }
   },
   // Data is the stuff our app cares about. We usually want to show this on the page.
