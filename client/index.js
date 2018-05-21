7d035c00d21cd84b11d725f2f1a4fd9f6e4e6feb
Andrew Eames <gamerweazel@gmail.com>
2018-05-21 11:07:04 -0600

    Programmatically updating data.

--- client/index.js
+++ client/index.js
@@ -2,9 +2,17 @@
 import data from './data.js'
 
 // Vue instance. Controls the app logic.
-new Vue({
+// We can do additional things with the instance if we give a variable name.
+const app = new Vue({
   // el, short for element. 'Hooks' into the div on index.html page
   el: '#app',
   // Data is the stuff our app cares about. We usually want to show this on the page.
   data
 })
+
+// Using our app variable, we can change things about our instance and the view will update accordingly.
+// This is known as one way data-binding, our view is bound to this data and updates when it changes.
+setTimeout(() => {
+  // We load the page and wait 2 seconds to change our app message.
+  app.message = 'Goodbye'
+}, 2000)
