1bc272b6b69761b1613f4812aea362d5b6f7eeb0
Andrew Eames <gamerweazel@gmail.com>
2018-05-23 09:44:16 -0600

    Client-side data validation is good for the user experience.

--- client/index.js
+++ client/index.js
@@ -8,20 +8,30 @@ const app = new Vue({
   el: '#app',
   // Methods are where we can associate pieces of code to do different things in our app.
   methods: {
+    isValid() {
+      const valid = (
+        (this.description && this.amount)
+        &&
+        (!isNaN(Number(this.amount)))
+      )
+      return valid
+    },
     // This method is handling the @click directive on our button in index.html.
     clicked(e) {
       // We have access to our data items by prepending 'this' before the name.
       // This code will add to our expenses list according to what was currently in the inputs when the user saved them.
       // Because description and amount were bound to inputs with v-model, they will contain whatever text is in the input.
-      this.expenses.unshift({
-        description: this.description,
-        // We have to 'cast' this field to a number in order to calculate the total correctly. It comes from the input as a string.
-        amount: Number(this.amount),
-        date: moment().format('MMMM Do, YYYY')
-      })
-      // Remember we can clear the inputs manually.
-      this.description = ''
-      this.amount = ''
+      if (this.isValid()) {
+        this.expenses.unshift({
+          description: this.description,
+          // We have to 'cast' this field to a number in order to calculate the total correctly. It comes from the input as a string.
+          amount: Number(this.amount),
+          date: moment().format('MMMM Do, YYYY')
+        })
+        // Remember we can clear the inputs manually.
+        this.description = ''
+        this.amount = ''
+      }
     }
   },
   // Computed things are different from data, we use our data to 'compute' new values that are stored in here.
