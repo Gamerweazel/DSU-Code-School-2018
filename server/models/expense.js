564fab13d680b633300f95603a23845614a42029
Andrew Eames <gamerweazel@gmail.com>
2018-05-24 15:53:04 -0600

    Moment on server side.

--- server/models/expense.js
+++ server/models/expense.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
+const moment = require('moment')
 
 const expenseSchema = mongoose.Schema({
   description: String,
   amount: Number,
-  date: { type: Date, default: Date.now }
+  date: { type: String, default: moment().format('MMMM Do, YYYY') }
 })
 
 module.exports = mongoose.model('Expense', expenseSchema)
